552f105ea775701f72f9b5d0323586cd86e2de87
Akshat <51688973+akshat-git@users.noreply.github.com>
2021-01-04 20:42:07 -0800

    Added functions and calls in respective .py files

--- drive_api_test.py
+++ drive_api_test.py
@@ -1,6 +1,6 @@
 '''
 This file contains the main code
-    - uses google spreadhseet api instance
+    - uses google spreadsheet api instance
     - takes in input for ticker and days
     - puts values of prices in sheet1
     - puts returns in sheet2
@@ -8,11 +8,22 @@ This file contains the main code
     - puts graph and data for each set of weightages in sheet4
 '''
 
+colors = {
+    'red':          [1,0,0,1],
+    'lightred':     [1,0,0,0.3],
+    'blue':         [0,0,1,1],
+    'lightblue':    [0,0,1,0.3],
+    'green':        [0,1,0,1],
+    'lightgreen':   [0,1,0,0.3],
+    'white':        [1,1,1,1],
+}
+
 from Google import Create_Service
 import pandas as pd
-# from ids import file_id
 from ids import *
-from functions import createstock,formatCells,sheetclear
+from functions import *
+import datetime
+
 # =============================================================================
 # Start drive related services
 API_NAME_DRIVE = 'drive'
@@ -25,17 +36,8 @@ API_VERSION_SHEET = 'v4'
 
 drive_service = Create_Service(CLIENT_SECRET_FILE_DRIVE, API_NAME_DRIVE, API_VERSION_DRIVE, SCOPES)
 sheet_service = Create_Service(CLIENT_SECRET_FILE_SHEET, API_NAME_SHEET, API_VERSION_SHEET, SCOPES)
-# =============================================================================
 
-# =============================================================================
-# Create a new folder for keeping all the files
-folder_metadata = {
-    'name': 'my_folder_for_sheets',
-    'mimeType': 'application/vnd.google-apps.folder'
-}
-# folder_prop = drive_service.files().create(body=folder_metadata).execute()
-# target_folder_id = folder_prop['id']
-# =============================================================================
+
 
 # =============================================================================
 # File and tickers
@@ -47,13 +49,14 @@ symbols = {
     "symbol01" : symbolin1, 
     "symbol02" : symbolin2
 }
-days = int(round(int(input("Working days: "))/5,0))
+days = int(input("Working days: "))
 skiprows = 0
 
 
 
 
 # =============================================================================
+# Names sheet 03
 names = {
     '':['Returns','St. Dev.','Sharpe']
 }
@@ -69,280 +72,41 @@ response_date = sheet_service.spreadsheets().values().update(
 
 
 
-lightblue = [143,121,0]
+
 
 # formatCells(startR, endR, startC, endC, sheetid, colors)
 # formatCells(2, 5, 1, 2, sheet03_id, lightblue)
 
+# conditional(sheet_id, colormin, colormid, colormax, startR, endR, startC, endC)
+conditional(sheet04_id, 4, colors['white'], colors['lightgreen'], colors['green'], 2, 23, 5, 6, sheet_service)
 
-
-
-
-names_graph = {
-    '':[symbols['symbol01'],'100%','95%','90%','85%','80%','75%','70%','65%','60%','55%','50%','45%','40%','35%','30%','25%','20%','15%','10%','5%','0%']
-}
-
-sheet_input_df = pd.DataFrame.from_dict(names_graph)
+date_col = list_dates(days)
+date_df = pd.DataFrame.from_dict(date_col)
 response_date = sheet_service.spreadsheets().values().update(
     spreadsheetId = file_id,
     valueInputOption = 'USER_ENTERED',
-    range = sheet04_name+'!B1',
+    range = sheet01_name+'!A2',
     body = dict(
         majorDimension = 'ROWS',
-        values = sheet_input_df.T.reset_index().T.values.tolist())
+        values = date_df.T.reset_index().T.values.tolist())
 ).execute()
-
-
-names_graph = {
-    '':[symbols['symbol02'],'=1-B3','=1-B4','=1-B5','=1-B6','=1-B7','=1-B8','=1-B9','=1-B10','=1-B11','=1-B12','=1-B13','=1-B14','=1-B15','=1-B16','=1-B17','=1-B18','=1-B19','=1-B20','=1-B21','=1-B22','=1-B23']
-}  
-sheet_input_df = pd.DataFrame.from_dict(names_graph)
 response_date = sheet_service.spreadsheets().values().update(
     spreadsheetId = file_id,
     valueInputOption = 'USER_ENTERED',
-    range = sheet04_name+'!C1',
+    range = sheet02_name+'!A3',
     body = dict(
         majorDimension = 'ROWS',
-        values = sheet_input_df.T.reset_index().T.values.tolist())
+        values = date_df.T.reset_index().T.values.tolist())
 ).execute()
 
-stdev01 ="'"+sheet03_name+"'!$C$4" 
-stdev02 ="'"+sheet03_name+"'!$D$4"
-sheet02_name_quote = "'"+sheet02_name+"'"
-names_graph = {
-    '':['St. Dev.',
-    "=SQRT((B3*" + stdev01 + ")^2+(C3*" + stdev02 + ")^2+(2*COVARIANCE.P(" + sheet02_name_quote + "!$B$4:" + sheet02_name_quote + "!$B$"+ str(days+2) + "," + sheet02_name_quote + "!$C$4:" + sheet02_name_quote + "!$C$"+ str(days+2) + ")*B3*C3))"]
-}   
-sheet_input_df = pd.DataFrame.from_dict(names_graph)
-response_date = sheet_service.spreadsheets().values().update(
-    spreadsheetId = file_id,
-    valueInputOption = 'USER_ENTERED',
-    range = sheet04_name+'!D1',
-    body = dict(
-        majorDimension = 'ROWS',
-        values = sheet_input_df.T.reset_index().T.values.tolist())
-).execute()
+# createstock(symbol, sheet, columnIndex, file_id, service,sheet2_id,sheet3_id,days)
+j = 1
+for i in symbols.values():
+    createstock(i, sheet01_name, sheet02_name, sheet03_name, j, file_id, sheet_service, sheet02_id, sheet03_id, days)
+    j += 1
 
-request_body_stdev_col = {
-    'requests':[
-        {
-            'repeatCell':{
-                'range':{
-                    "sheetId": sheet04_id,
-                    "startRowIndex": 2,
-                    "endRowIndex": 23,
-                    "startColumnIndex": 3,
-                    "endColumnIndex": 4
-                },
-                'cell':{
-                    "userEnteredValue": {
-                        "formulaValue": names_graph[''][1]
-                    },
-                    "userEnteredFormat": {
-                        'numberFormat':{
-                            'type':'PERCENT',
-                            'pattern':'0.##%'
-                        },
-                        'borders':{
-                            "top":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "bottom":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "right":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "left":{
-                                "style": 'SOLID_MEDIUM'
-                            }
-                        } 
-                    }
-                },
-                'fields':'*'
-            }
-        }
-
-    ]
-}
-response_date = sheet_service.spreadsheets().batchUpdate(
-    spreadsheetId = file_id,
-    body = request_body_stdev_col
-).execute()
-sheet03_name_quote = "'"+sheet03_name+"'!"
-names_graph = { 
-    '':['Returns',"=sumproduct(B3:C3,"+sheet03_name_quote+"$C$3:"+sheet03_name_quote+"$D$3)"]
-}   
-
-sheet_input_df = pd.DataFrame.from_dict(names_graph)
-response_date = sheet_service.spreadsheets().values().update(
-    spreadsheetId = file_id,
-    valueInputOption = 'USER_ENTERED',
-    range = sheet04_name+'!E1',
-    body = dict(
-        majorDimension = 'ROWS',
-        values = sheet_input_df.T.reset_index().T.values.tolist())
-).execute()
-
-request_body_return_col = {
-    'requests':[
-        {
-            'repeatCell':{
-                'range':{
-                    "sheetId": sheet04_id,
-                    "startRowIndex": 2,
-                    "endRowIndex": 23,
-                    "startColumnIndex": 4,
-                    "endColumnIndex": 5
-                },
-                'cell':{
-                    "userEnteredValue": {
-                        "formulaValue": names_graph[''][1]
-                    },
-                    "userEnteredFormat": {
-                        'numberFormat':{
-                            'type':'PERCENT',
-                            'pattern':'0.##%'
-                        },
-                        'borders':{
-                            "top":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "bottom":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "right":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "left":{
-                                "style": 'SOLID_MEDIUM'
-                            }
-                        } 
-                    }
-                },
-                'fields':'*'
-            }
-        }
-    ]
-}
-response_date = sheet_service.spreadsheets().batchUpdate(
-    spreadsheetId = file_id,
-    body = request_body_return_col
-).execute()
-names_graph = {
-    '':['Sharpe','=E3/D3']
-}
-sheet_input_df = pd.DataFrame.from_dict(names_graph)
-response_date = sheet_service.spreadsheets().values().update(
-    spreadsheetId = file_id,
-    valueInputOption = 'USER_ENTERED',
-    range = sheet04_name+'!F1',
-    body = dict(
-        majorDimension = 'ROWS',
-        values = sheet_input_df.T.reset_index().T.values.tolist())
-).execute()
-
-request_body_sharpe_col = {
-    'requests':[
-        {
-            'repeatCell':{
-                'range':{
-                    "sheetId": sheet04_id,
-                    "startRowIndex": 2,
-                    "endRowIndex": 23,
-                    "startColumnIndex": 5,
-                    "endColumnIndex": 6
-                },
-                'cell':{
-                    "userEnteredValue": {
-                        "formulaValue": names_graph[''][1]
-                    },
-                    "userEnteredFormat": {
-                        'numberFormat':{
-                            'type':'NUMBER',
-                            'pattern':'0.###'
-                        },
-                        'borders':{
-                            "top":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "bottom":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "right":{
-                                "style": 'SOLID_MEDIUM'
-                            },
-                            "left":{
-                                "style": 'SOLID_MEDIUM'
-                            }
-                        }  
-                    }
-                },
-                'fields':'*'
-            }
-        }
-    ]
-}
-response_date = sheet_service.spreadsheets().batchUpdate(
-    spreadsheetId = file_id,
-    body = request_body_sharpe_col
-).execute()
-
-
-request_body_cond = {
-    'requests':[
-        {
-            'addConditionalFormatRule': {
-                'rule': {
-                    'ranges':[
-                        {
-                            "sheetId": sheet04_id,
-                            "startRowIndex": 2,
-                            "endRowIndex": 23,
-                            "startColumnIndex": 5,
-                            "endColumnIndex": 6
-                        }
-                    ],
-                    'gradientRule':{
-                        "minpoint": {
-                            "color": {
-                                "red": 1,
-                                "green": 1,
-                                "blue": 1,
-                                "alpha": 0.3
-                            },
-                            "type":"MIN" 
-                        },
-                        "midpoint": {
-                            "color": {
-                                "red": 0.6,
-                                "green": 1,
-                                "blue": 0.6,
-                                "alpha": 1
-                            },
-                            "type": "PERCENT" ,
-                            "value": "80"
-                        },
-                        "maxpoint": {
-                            "color": {
-                                "red": 0,
-                                "green": 1,
-                                "blue": 0,  
-                                "alpha": 1
-                            },
-                            "type": "MAX" 
-                        }
-                    }
-                },
-                'index': 0
-            }
-        }
-    ]
-}
-response_date = sheet_service.spreadsheets().batchUpdate(
-        spreadsheetId = file_id,
-        body = request_body_cond
-).execute()
+# sheet04(sheet02_name, sheet03_name, sheet04_name, sheet04_id, file_id)
+sheet04(sheet02_name, sheet03_name, sheet04_name, sheet04_id, file_id, symbols,sheet_service,days)
 
 
 tangline = {
@@ -361,14 +125,6 @@ response_date = sheet_service.spreadsheets().values().update(
 ).execute()
 '''
 
-# createstock(symbol, sheet, columnIndex, file_id, service,sheet2_id,sheet3_id,days)
-j = 1
-for i in symbols.values():
-    createstock(i, sheet01_name, sheet02_name, sheet03_name, j, file_id, sheet_service, sheet02_id, sheet03_id, days)
-    j += 1
-
-
-# =============================================================================
 
 # =============================================================================
 # Plot chart
--- functions.py
+++ functions.py
@@ -1,19 +1,24 @@
 '''
-3 different functions:
-    createstock
-        puts column by column info in sheet 1-3
-    formatCells
-        formats cells
-    sheetclear
-        clears
+Different functions
 '''
 
 import pandas as pd
 from ids import *
+import datetime
+
+def dates_bwn_twodates(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + datetime.timedelta(n)
+def list_dates(days_length):
+    sdate = datetime.date(2011,1,1)
+    edate = datetime.date(2021,1,1)
+    datelist = list(reversed([str(d) for d in dates_bwn_twodates(sdate,edate)]))[:days_length]
+    dates = {'Date':datelist}
+    return dates
 def createstock(symbol, sheet, sheet2, sheet3, columnIndex, file_id, service,sheet2_id,sheet3_id,days):
     columns = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     column = columns[columnIndex]
-    url = "https://query1.finance.yahoo.com/v7/finance/download/"+ symbol +"?period1=1451520000&period2=1609372800&interval=1wk&events=history&includeAdjustedClose=true"
+    url = "https://query1.finance.yahoo.com/v7/finance/download/" + symbol + "?period1=1293840000&period2=1609459200&interval=1d&events=history&includeAdjustedClose=true"
     stock_price = pd.read_csv(url, skiprows = 0)
     new_set = stock_price['Close']
     new_set = dict(new_set.iloc[::-1])
@@ -180,7 +185,6 @@ def createstock(symbol, sheet, sheet2, sheet3, columnIndex, file_id, service,she
             majorDimension = 'ROWS',
             values = sheet_input_df.T.reset_index().T.values.tolist())
     ).execute()
-
 def formatCells(startR, endR, startC, endC, sheetid, colors):
     request_body_format_cells = {
         'requests': [
@@ -213,7 +217,6 @@ def formatCells(startR, endR, startC, endC, sheetid, colors):
         spreadsheetId = file_id,
         body = request_body_format_cells
     ).execute()
-
 def sheetclear(service):
     request_body_clear = {
         'requests':[
@@ -261,4 +264,241 @@ def sheetclear(service):
     service.spreadsheets().batchUpdate(
         spreadsheetId = file_id,
         body = request_body_clear
+    ).execute()
+def conditional(sheet_id, percentile, colormin, colormid, colormax, startR, endR, startC, endC, sheet_service):
+    request_body_cond = {
+        'requests':[
+            {
+                'addConditionalFormatRule': {
+                    'rule': {
+                        'ranges':[
+                            {
+                                "sheetId": sheet04_id,
+                                "startRowIndex": startR,
+                                "endRowIndex": endR,
+                                "startColumnIndex": startC,
+                                "endColumnIndex": endC
+                            }
+                        ],
+                        'gradientRule':{
+                            "minpoint": {
+                                "color": {
+                                    "red": colormin[0],
+                                    "green": colormin[1],
+                                    "blue": colormin[2],
+                                    "alpha": colormin[3]
+                                },
+                                "type":"MIN" 
+                            },
+                            "midpoint": {
+                                "color": {
+                                    "red": colormid[0],
+                                    "green": colormid[1],
+                                    "blue": colormid[2],
+                                    "alpha": colormid[3]
+                                },
+                                "type": "PERCENT" ,
+                                "value": str(percentile)
+                            },
+                            "maxpoint": {
+                                "color": {
+                                    "red": colormax[0],
+                                    "green": colormax[1],
+                                    "blue": colormax[2],  
+                                    "alpha": colormax[3]
+                                },
+                                "type": "MAX" 
+                            }
+                        }
+                    },
+                    'index': 0
+                }
+            }
+        ]
+    }
+    response_date = sheet_service.spreadsheets().batchUpdate(
+            spreadsheetId = file_id,
+            body = request_body_cond
+    ).execute()
+def sheet04(sheet02_name, sheet03_name, sheet04_name, sheet04_id, file_id,symbollist,sheet_service,days):
+    names_graph = {
+        '':[symbollist['symbol01'],'100%','95%','90%','85%','80%','75%','70%','65%','60%','55%','50%','45%','40%','35%','30%','25%','20%','15%','10%','5%','0%']
+    }
+
+    sheet_input_df = pd.DataFrame.from_dict(names_graph)
+    response_date = sheet_service.spreadsheets().values().update(
+        spreadsheetId = file_id,
+        valueInputOption = 'USER_ENTERED',
+        range = sheet04_name+'!B1',
+        body = dict(
+            majorDimension = 'ROWS',
+            values = sheet_input_df.T.reset_index().T.values.tolist())
+    ).execute()
+
+
+    names_graph = {
+        '':[symbollist['symbol02'],'=1-B3','=1-B4','=1-B5','=1-B6','=1-B7','=1-B8','=1-B9','=1-B10','=1-B11','=1-B12','=1-B13','=1-B14','=1-B15','=1-B16','=1-B17','=1-B18','=1-B19','=1-B20','=1-B21','=1-B22','=1-B23']
+    }  
+    sheet_input_df = pd.DataFrame.from_dict(names_graph)
+    response_date = sheet_service.spreadsheets().values().update(
+        spreadsheetId = file_id,
+        valueInputOption = 'USER_ENTERED',
+        range = sheet04_name+'!C1',
+        body = dict(
+            majorDimension = 'ROWS',
+            values = sheet_input_df.T.reset_index().T.values.tolist())
+    ).execute()
+
+    stdev01 ="'"+sheet03_name+"'!$C$4" 
+    stdev02 ="'"+sheet03_name+"'!$D$4"
+    sheet02_name_quote = "'"+sheet02_name+"'"
+    names_graph = {
+        '':['St. Dev.',
+        "=SQRT((B3*" + stdev01 + ")^2+(C3*" + stdev02 + ")^2+(2*COVARIANCE.P(" + sheet02_name_quote + "!$B$4:" + sheet02_name_quote + "!$B$"+ str(days+2) + "," + sheet02_name_quote + "!$C$4:" + sheet02_name_quote + "!$C$"+ str(days+2) + ")*B3*C3))"]
+    }   
+    sheet_input_df = pd.DataFrame.from_dict(names_graph)
+    response_date = sheet_service.spreadsheets().values().update(
+        spreadsheetId = file_id,
+        valueInputOption = 'USER_ENTERED',
+        range = sheet04_name+'!D1',
+        body = dict(
+            majorDimension = 'ROWS',
+            values = sheet_input_df.T.reset_index().T.values.tolist())
+    ).execute()
+
+    request_body_stdev_col = {
+        'requests':[
+            {
+                'repeatCell':{
+                    'range':{
+                        "sheetId": sheet04_id,
+                        "startRowIndex": 2,
+                        "endRowIndex": 23,
+                        "startColumnIndex": 3,
+                        "endColumnIndex": 4
+                    },
+                    'cell':{
+                        "userEnteredValue": {
+                            "formulaValue": names_graph[''][1]
+                        },
+                        "userEnteredFormat": {
+                            'numberFormat':{
+                                'type':'PERCENT',
+                                'pattern':'0.##%'
+                            }
+                        }
+                    },
+                    'fields':'*'
+                }
+            }
+
+        ]
+    }
+    response_date = sheet_service.spreadsheets().batchUpdate(
+        spreadsheetId = file_id,
+        body = request_body_stdev_col
+    ).execute()
+    sheet03_name_quote = "'"+sheet03_name+"'!"
+    names_graph = { 
+        '':['Returns',"=sumproduct(B3:C3,"+sheet03_name_quote+"$C$3:"+sheet03_name_quote+"$D$3)"]
+    }   
+
+    sheet_input_df = pd.DataFrame.from_dict(names_graph)
+    response_date = sheet_service.spreadsheets().values().update(
+        spreadsheetId = file_id,
+        valueInputOption = 'USER_ENTERED',
+        range = sheet04_name+'!E1',
+        body = dict(
+            majorDimension = 'ROWS',
+            values = sheet_input_df.T.reset_index().T.values.tolist())
+    ).execute()
+
+    request_body_return_col = {
+        'requests':[
+            {
+                'repeatCell':{
+                    'range':{
+                        "sheetId": sheet04_id,
+                        "startRowIndex": 2,
+                        "endRowIndex": 23,
+                        "startColumnIndex": 4,
+                        "endColumnIndex": 5
+                    },
+                    'cell':{
+                        "userEnteredValue": {
+                            "formulaValue": names_graph[''][1]
+                        },
+                        "userEnteredFormat": {
+                            'numberFormat':{
+                                'type':'PERCENT',
+                                'pattern':'0.##%'
+                            }
+                        }
+                    },
+                    'fields':'*'
+                }
+            }
+        ]
+    }
+    response_date = sheet_service.spreadsheets().batchUpdate(
+        spreadsheetId = file_id,
+        body = request_body_return_col
+    ).execute()
+    names_graph = {
+        '':['Sharpe','=E3/D3']
+    }
+    sheet_input_df = pd.DataFrame.from_dict(names_graph)
+    response_date = sheet_service.spreadsheets().values().update(
+        spreadsheetId = file_id,
+        valueInputOption = 'USER_ENTERED',
+        range = sheet04_name+'!F1',
+        body = dict(
+            majorDimension = 'ROWS',
+            values = sheet_input_df.T.reset_index().T.values.tolist())
+    ).execute()
+
+    request_body_sharpe_col = {
+        'requests':[
+            {
+                'repeatCell':{
+                    'range':{
+                        "sheetId": sheet04_id,
+                        "startRowIndex": 2,
+                        "endRowIndex": 23,
+                        "startColumnIndex": 5,
+                        "endColumnIndex": 6
+                    },
+                    'cell':{
+                        "userEnteredValue": {
+                            "formulaValue": names_graph[''][1]
+                        },
+                        "userEnteredFormat": {
+                            'numberFormat':{
+                                'type':'NUMBER',
+                                'pattern':'0.###'
+                            },
+                            'borders':{
+                                "top":{
+                                    "style": 'SOLID_MEDIUM'
+                                },
+                                "bottom":{
+                                    "style": 'SOLID_MEDIUM'
+                                },
+                                "right":{
+                                    "style": 'SOLID_MEDIUM'
+                                },
+                                "left":{
+                                    "style": 'SOLID_MEDIUM'
+                                }
+                            }  
+                        }
+                    },
+                    'fields':'*'
+                }
+            }
+        ]
+    }
+    response_date = sheet_service.spreadsheets().batchUpdate(
+        spreadsheetId = file_id,
+        body = request_body_sharpe_col
     ).execute()
